b3b8a3c1ccedbca05a822825469674addbf9257f
Gayathridevi-123 <gayubhuva53@gmail.com>
2025-10-26 02:08:42 +0530

    updated nostra project

---
+++ product.js
@@ -0,0 +1,83 @@
+const products = [
+    {
+        id:1,
+        name:"Floral Summer Shirt",
+        src:"/images/products/n5.png",
+        desc:"",
+        price:220,
+        tags:["new","blue","summer"]
+    },
+
+    {
+        id:2,
+        name:"Summer Green",
+        src:"/images/products/f7.png",
+        desc:"",
+        price:260,
+        tags:["new","green","beach"]
+    },
+
+    {
+        id:3,
+        name:"Party Floral Shirt",
+        src:"/images/products/f6.png",
+        desc:"",
+        price:399,
+        tags:["old","red","party"]
+    },
+
+    {
+        id:4,
+        name:"Floral Summer Shirt",
+        src:"/images/products/f8.png",
+        desc:"",
+        price:399,
+        tags:["old","white","beach"]
+    },
+    {
+        id:5,
+        name:"Floral summer dress",
+        src:"/images/products/n6.png",
+        desc:"",
+        price:579,
+        tags:["old","white","beach"]
+    }
+,
+    {
+        id:6,
+        name:"Shirt Party Red",
+        src:"/images/products/n7.png",
+        desc:"",
+        price:579,
+        tags:["old","red","party"]
+    },
+
+    {
+        id:7,
+        name:"Party Floral Shirt",
+        src:"/images/products/n3.png",
+        desc:"",
+        price:399,
+        tags:["old","red","party"]
+    },
+    {
+        id:8,
+        name:"Shirt Party Red",
+        src:"/images/products/n2.png",
+        desc:"",
+        price:579,
+        tags:["old","red","party"]
+    },
+    {
+        id:9,
+        name:"Floral Summer Shirt",
+        src:"/images/products/f4.png",
+        desc:"",
+        price:220,
+        tags:["new","blue","summer"]
+    },
+
+
+]
+
+export {products}
